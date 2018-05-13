a11e325239eba9f3438c847f57e6686013addeff
Nicolas Pierson <nicolas.pierson@cornerside.fr>
2018-05-13 20:48:35 +0200

    Fix syntax

--- aws-creds-export.sh
+++ aws-creds-export.sh
@@ -5,8 +5,6 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   exit 1
 fi
 
-CONF_FILE=~/.aws/credentials
-
 function usage {
   echo "Automates the process of obtaining temporary credentials from the "
   echo "AWS Security Token Service and export the AWS API keys."
@@ -34,20 +32,28 @@ if [ -z "$PROFILE" ]; then
   return 1
 fi
 
-aws-mfa --profile $PROFILE
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_SESSION_TOKEN=
+AWS_DEFAULT_REGION=
 
-if [ $? -ne 0  ]; then
+if ! aws-mfa --profile "$PROFILE"; then
   echo "Invalid authentication. Please try again."
   return 1
 fi
 
-export AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id --profile $PROFILE)"
-export AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key --profile $PROFILE)"
-export AWS_SESSION_TOKEN="$(aws configure get aws_session_token --profile $PROFILE)"
-export AWS_DEFAULT_REGION="$(aws configure get region --profile $PROFILE)"
+AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile "$PROFILE")
+AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile "$PROFILE")
+AWS_SESSION_TOKEN=$(aws configure get aws_session_token --profile "$PROFILE")
+AWS_DEFAULT_REGION=$(aws configure get region --profile "$PROFILE")
+
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
+export AWS_DEFAULT_REGION
 
 echo "Profile: $PROFILE"
-echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-echo AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY|tr '[:print:]' '*')
-echo AWS_SESSION_TOKEN=$(echo $AWS_SESSION_TOKEN|tr '[:print:]' '*')
-echo AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY=$(echo "$AWS_SECRET_ACCESS_KEY"|tr '[:print:]' '*')"
+echo "AWS_SESSION_TOKEN=$(echo "$AWS_SESSION_TOKEN"|tr '[:print:]' '*')"
+echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION"
